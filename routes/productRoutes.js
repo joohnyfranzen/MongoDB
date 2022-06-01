be5a16395385a1af11b0b2fec59a537b5e53f1cd
Jonathan <joohny_2014@hotmail.com>
2022-06-01 18:11:00 -0300

    primeira rota

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 const ProductController = require('../controllers/ProductController')
 
+router.get('/create', ProductController.createProduct)
 router.get('/', ProductController.showProducts)
 
 module.exports = router
