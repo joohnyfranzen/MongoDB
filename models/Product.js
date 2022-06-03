b07e592ca71e62a80b930f032fedf8df155361db
Jonathan <joohny_2014@hotmail.com>
2022-06-03 18:45:21 -0300

    Creating new database

--- models/Product.js
+++ models/Product.js
@@ -1,58 +1,70 @@
-const conn = require('../db/conn')
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+const Product = mongoose.model(
+    'Product',
+    new Schema({
+        name: { type: String, required: true },
+        price: { type: Number, required: true },
+        description: { type: String, required: true },
+        image: { type: String, required: true },
+    })
+)
 
-const { ObjectId } = require('mongodb')
+// const conn = require('../db/conn')
 
-class Product {
+// const { ObjectId } = require('mongodb')
+
+// class Product {
     
-    constructor(name, image, price, description) {
+//     constructor(name, image, price, description) {
 
-        this.name = name
-        this.image = image
-        this.price = price
-        this.description = description
+//         this.name = name
+//         this.image = image
+//         this.price = price
+//         this.description = description
 
-    }
-    save() {
+//     }
+//     save() {
 
-        const product = conn.db().collection('products').insertOne({
-            name: this.name,
-            image: this.image,
-            price: this.price,
-            description: this.description,
-        })
+//         const product = conn.db().collection('products').insertOne({
+//             name: this.name,
+//             image: this.image,
+//             price: this.price,
+//             description: this.description,
+//         })
 
-        return product
-    }
+//         return product
+//     }
 
-    static getProducts() {
+//     static getProducts() {
         
-        const products = conn.db().collection('products').find().toArray()
+//         const products = conn.db().collection('products').find().toArray()
 
-        return products
-    }
+//         return products
+//     }
 
-    static async getProductById(id) {
+//     static async getProductById(id) {
 
-        const product = await conn.db().collection('products').findOne({ _id: ObjectId(id) })
+//         const product = await conn.db().collection('products').findOne({ _id: ObjectId(id) })
     
-        return product
-    }
-    static async removeProductById(id) {
+//         return product
+//     }
+//     static async removeProductById(id) {
 
-        const product = await conn.db().collection('products').deleteOne({ _id: ObjectId(id) })
+//         const product = await conn.db().collection('products').deleteOne({ _id: ObjectId(id) })
 
-        return
-    }
+//         return
+//     }
 
-    updateProduct(id) {
+//     updateProduct(id) {
     
-        conn
-            .db()
-            .collection('products')
-            .updateOne({ _id: ObjectId(id)}, {$set: this })
+//         conn
+//             .db()
+//             .collection('products')
+//             .updateOne({ _id: ObjectId(id)}, {$set: this })
 
-        return
-    }
-} 
+//         return
+//     }
+// } 
 
 module.exports = Product
